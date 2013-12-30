a5aa26b231ec9cf2597b4d9678f9fde3f4373deb
kaibrusch <kaibrusch@gmail.com>
2013-12-30 15:05:57 +0100

    first succ test

--- A4/src/a10/gastkomponente/verwalter/Gastverwalter.java
+++ A4/src/a10/gastkomponente/verwalter/Gastverwalter.java
@@ -1,5 +1,13 @@
 package a10.gastkomponente.verwalter;
+import persistenz.*;
 
 public class Gastverwalter {
+	
+		  public static void main(String[] args) throws Exception {
+		    SqlConnecter connection = new SqlConnecter();
+		    connection.readDataBase();
+		    
+		  }
+
 
 }

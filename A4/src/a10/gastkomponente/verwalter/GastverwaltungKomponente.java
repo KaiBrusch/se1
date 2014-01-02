55a7ecb760c31bcec2854080776d2c93bb0084c0
sacry- <nitsche25@gmail.com>
2014-01-02 12:20:27 +0100

    Verwaltungsstuff

--- A4/src/a10/gastkomponente/verwalter/GastverwaltungKomponente.java
+++ A4/src/a10/gastkomponente/verwalter/GastverwaltungKomponente.java
@@ -1,11 +1,21 @@
 package a10.gastkomponente.verwalter;
 
+import persistenz.IPersistenzService;
 import a10.gastkomponente.Email;
 import a10.gastkomponente.Gast;
 import a10.gastkomponente.IGastServices;
 
 public class GastverwaltungKomponente implements IGastServices {
 
+	private Gastverwalter gastVerwalter = null;
+	private GastverwaltungAnwendungsfall gastVerwaltungAnwendungsfall = null;
+	
+	public GastverwaltungKomponente(IPersistenzService persistenceManager)
+    {
+		gastVerwalter = new Gastverwalter(persistenceManager);
+		gastVerwaltungAnwendungsfall = new GastverwaltungAnwendungsfall(gastVerwalter);
+    }
+	
 	@Override
 	public Gast erzeugeGast(Integer nr, String name, Email email) {
 		// TODO Auto-generated method stub

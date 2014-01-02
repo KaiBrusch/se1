4f579778462bf854e06d8defbed1d257cc422ab6
sacry- <nitsche25@gmail.com>
2014-01-02 17:13:54 +0100

    starting object injection/composition

--- A4/src/a10/Main.java
+++ A4/src/a10/Main.java
@@ -1,44 +1,43 @@
 package a10;
 
 import persistenz.IPersistenzService;
+import persistenz.SqlConnecter;
 import a10.gastkomponente.*;
 import a10.gastkomponente.verwalter.GastverwaltungKomponente;
 
 public class Main {
 
-	// - Zunächst werden Stammdaten angelegt (ErzeugeGast und
-	// ErzeugeZusatzleistung). Zusatzleistungen sind bspw. „Sauna“,
-	// „Vollpension“, „WLAN“.
+	// - Zuna��chst werden Stammdaten angelegt (ErzeugeGast und
+	// ErzeugeZusatzleistung). Zusatzleistungen sind bspw. ���Sauna���,
+	// ���Vollpension���, ���WLAN���.
 	// - Eine neue Reservierung kann mittels ReserviereZimmer erzeugt werden. Es
-	// wird ein Reservierungsobjekt mit eindeutiger Nr zurückgegeben.
-	// - Auf vorhandenen Reservierungen können mittels BucheZusatzleistung
+	// wird ein Reservierungsobjekt mit eindeutiger Nr zuru��ckgegeben.
+	// - Auf vorhandenen Reservierungen ko��nnen mittels BucheZusatzleistung
 	// Zusatzleistungen gebucht werden.
-	// - Falls ein Gast mehr als 5 Reservierungen getätigt, oder er mindestens
+	// - Falls ein Gast mehr als 5 Reservierungen geta��tigt, oder er mindestens
 	// 3 Reservierungen mit Zusatzleistungen gebucht hat, wird er in der
 	// GastKomponente als Stammgast markiert (der Aufruf erfolgt durch die
-	// ReservierungsKomponente im Zuge der Ausführung der Operation
+	// ReservierungsKomponente im Zuge der Ausfu��hrung der Operation
 	// ReserviereZimmer bzw. BucheZusatzleistung)
-
 
 	private static IGastServices gastService = null;
 
 	private static IPersistenzService persistenceService = null;
 
 	public static void main(String[] args) {
-		gastService = new GastverwaltungKomponente();
-		
-		
-		
 		Integer nr = 1;
 		String name = "matthias";
-		Boolean stammkunde = true;
 		Email email = Email.email(name, "gmail", "de");
-		Gast g = new Gast(nr, name, email, stammkunde);
-		System.out.println(g);
-		// Gast g = ErzeugeGast(nr, name, email);
-		// Gast SucheGastNachName(String name);
-		// ReserviereZimmer();
-		// BucheZusatzleistung();
+		// Gast
+		IPersistenzService persistenceService = new SqlConnecter();
+		IGastServices gastService = new GastverwaltungKomponente(
+				persistenceService);
+		gastService.erzeugeGast(nr, name, email);
+		Gast matze = gastService.sucheGastNachName("matthias");
+		// Zusatzleistung Sauna, Vollpension, WLAN
+		// Reservierung
+		// BucheZusatzleistung (Assertion 5 Reservierungen || 3 reservierungen +
+		// zusatzleistung
 
 	}
 }
